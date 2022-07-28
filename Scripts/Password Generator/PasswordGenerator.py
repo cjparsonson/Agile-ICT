eb905a4f2f49282d2714c97c769808a8089ce9b0
Chris Parsonson <chris.parsonson@agileict.co.uk>
2022-07-28 12:52:40 +0100

    Update script

--- Scripts/Password Generator/PasswordGenerator.py
+++ Scripts/Password Generator/PasswordGenerator.py
@@ -2,13 +2,15 @@
 
 # Combines two five letter words and two trailing digits.
 
-from itertools import count
+# Imports
 import random
 
+# Open Word List
 with open("Scripts\Password Generator/fiveLetterWords.txt") as text:
     Word_List = text.readlines()
     Word_List = [word.strip() for word in Word_List] 
 
+# Function - generates password by joining two random 5 letter words and a 2 digit number
 def Generate_Password() -> str: 
     Word_Choices = random.choices(Word_List, k=2)
 
@@ -16,6 +18,7 @@ def Generate_Password() -> str:
     Password = f"{Pass_Text}{random.randint(10,99)}"
     return Password
 
+# Checks password list for duplicate entries. Outputs True is the list contains only unique values.
 def Check_Duplicates(input_list: list) -> bool:    
     unique = []
     duplicates = []
@@ -30,9 +33,21 @@ def Check_Duplicates(input_list: list) -> bool:
         return False    
 
 
+# Set password list variable
 Password_List = []
-for i in range(5):
-    Password_List.append(Generate_Password())
+# Get number of passwords to generate
+Password_Range = int(input("Please specify the number of passwords to be generated: "))
 
-print(Check_Duplicates(Password_List))
+# Generate passwords 
+for i in range(Password_Range):
+    Password_List.append(Generate_Password() + "\n")
+
+# Check for duplicates and print result
+List_Valid = Check_Duplicates(Password_List)
+print(List_Valid)
+
+# If list contains only unique passwords, output to .txt file (Caution - Overwrites)
+if List_Valid:
+    with open("Scripts\Password Generator/passwords.txt", "w") as output_file:
+        output_file.writelines(Password_List)
 
