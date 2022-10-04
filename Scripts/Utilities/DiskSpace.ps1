e809e8509ce6873a372311a83e7c4efee87e8005
cjparsonson <cjparsonson@gmail.com>
2022-10-04 15:41:42 +0100

    Updates

---
+++ Scripts/Utilities/DiskSpace.ps1
@@ -0,0 +1,8 @@
+# 
+
+Get-CimInstance -ClassName Win32_LogicalDisk -ComputerName localhost `
+-Filter "drivetype=3" | Sort-Object -Property DeviceID |
+Format-Table -Property DeviceID,
+@{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
+@{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
+@{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
