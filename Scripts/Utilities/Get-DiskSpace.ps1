36cec89c8453360ec42bac778743782178717187
cjparsonson <cjparsonson@gmail.com>
2022-10-06 15:04:24 +0100

    Updates

--- Scripts/Utilities/Get-DiskSpace.ps1
+++ Scripts/Utilities/Get-DiskSpace.ps1
@@ -1,11 +1,25 @@
-# Get disks, size and freespace.
+<#
+.SYNOPSIS
+Get-DiskSpace retrives disk information from one or more computers.
+.DESCRIPTION
+Get-DiskSpace uses CIM to retrieve the Win32_LogicalDisk instances from one or more computers. It displays
+each disks drive letter, free space, total size and % free.
+.PARAMETER ComputerName
+The computer name, or names, to query. Default: LocalHost
+.PARAMETER DriveType
+The drive type to query. See Win32_LogicalDisk documentation for values.
+3 is a fixed disk. Default: 3
+.EXAMPLE
+Get-DiskSpace -ComputerName SRV01 -DriveType 3
+#>
 
 param (
-    $ComputerName = 'LocalHost'
+    $ComputerName = 'LocalHost',
+    $DriveType = 3
 )
 
 Get-CimInstance -ClassName Win32_LogicalDisk -ComputerName $ComputerName `
--Filter "drivetype=3" | Sort-Object -Property DeviceID |
+-Filter "drivetype=$DriveType" | Sort-Object -Property DeviceID |
 Format-Table -Property DeviceID,
 @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
 @{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
