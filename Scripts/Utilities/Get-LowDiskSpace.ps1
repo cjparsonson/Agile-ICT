c3274c5a5805781a1888c35f756a62b2c0227a44
cjparsonson <cjparsonson@gmail.com>
2022-10-06 17:02:11 +0100

    Updates

---
+++ Scripts/Utilities/Get-LowDiskSpace.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+Get-LowDiskSpace returns drives from one or more computers with less than a given threshold of free disk space.
+.DESCRIPTION
+Get-LowDiskSpace uses CIM to retrieve the Win32_LogicalDisk instances from one or more computers. It displays
+each disks drive letter, free space, total size and % free. This script will only return disks which have
+free space less than a given threshold.
+.PARAMETER computername
+Specify one or more computers. Default: LocalHost
+.PARAMETER percentagethreshold
+Specify percentage threshold. Default: 0.05 (5%)
+.EXAMPLE
+Get-LowDiskSpace -computername SRV01 -percentagethreshold 0.1
+#>
+
+param (
+    $computername = 'LocalHost',
+    $percentagethreshold = 0.05
+)
+
+Get-CimInstance -ClassName Win32_LogicalDisk -ComputerName $computername -Filter "drivetype=3" |
+Where-Object -FilterScript { ($_.FreeSpace / $_.Size) -lt $percentagethreshold } |
+Format-Table -Property DeviceID,
+@{label='FreeSpace(GB)';expression={$_.FreeSpace / 1GB -as [int]}},
+@{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
+@{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
