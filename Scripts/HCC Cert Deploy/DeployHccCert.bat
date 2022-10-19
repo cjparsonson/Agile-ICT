505d75666e8132a4076c1a945bd70c1a972e21af
cjparsonson <cjparsonson@gmail.com>
2022-10-19 15:26:54 +0100

    Update

--- Scripts/HCC Cert Deploy/DeployHccCert.bat
+++ Scripts/HCC Cert Deploy/DeployHccCert.bat
@@ -2,9 +2,9 @@ REM Users cert util to deploy new certificates from a network share
 
 @echo deploying new certificates
 
-certutil -enterprise -addstore -f "root" "\\%userdomain%\dfs$\mansoft\Core\HCCCert\CrossSigned\3334561877.crt"
+certutil -enterprise -addstore -f "root" "\\%userdomain%\dfs$\Scripts\HCCCertDeploy\3334561877.crt"
 
-certutil -enterprise -addstore -f "root" "\\%userdomain%\dfs$\mansoft\Core\HCCCert\CrossSigned\3334561878.crt"
+certutil -enterprise -addstore -f "root" "\\%userdomain%\dfs$\Scripts\HCCCertDeploy\3334561878.crt"
 
-certutil -enterprise -addstore -f "root" "\\%userdomain%\dfs$\mansoft\Core\HCCCert\CrossSigned\3334561879.crt"
+certutil -enterprise -addstore -f "root" "\\%userdomain%\dfs$\Scripts\HCCCertDeploy\3334561879.crt"
 
