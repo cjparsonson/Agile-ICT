63bb0d33e0349ef7154613c78187eadeb1c34ead
cjparsonson <cjparsonson@gmail.com>
2022-11-15 08:34:17 +0000

    Secure

--- Scripts/InventryMap/InventryMap.bat
+++ Scripts/InventryMap/InventryMap.bat
@@ -1,6 +1,6 @@
 REM net use I: \\networkShare\Test /u:domainname\username password
 
 @echo Create new I: Drive Mapping
-net use I: \\10.201.80.106\inventry\v4\console /u:10.201.80.106\Inventry Inventry1983 /persistent:yes
+net use I: \\10.201.80.106\inventry\v4\console /u:10.201.80.106\Inventry Inventry1*** /persistent:yes
 exit
 
