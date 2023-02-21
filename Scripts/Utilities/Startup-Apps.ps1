59cc75d6702126db0dfcf0d1c9871a2e565e6a88
cjparsonson <cjparsonson@gmail.com>
2023-02-21 11:53:07 +0000

    Add script

---
+++ Scripts/Utilities/Startup-Apps.ps1
@@ -0,0 +1,10 @@
+# Script to open programs at startup
+
+& "C:\Users\chris.parsonson.TEAMORANGE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Outlook.lnk"
+
+& "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
+
+& "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OneNote.lnk"
+
+& "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.16.10262.0_x64__8wekyb3d8bbwe\WindowsTerminal.exe"
+
