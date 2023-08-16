363ba06123917e75c9fadb4e0a2156c93afe9ce4
cjparsonson <cjparsonson@gmail.com>
2023-08-16 16:58:56 +0100

    Updates

--- Scripts/Utilities/Get-WsusUpdates.ps1
+++ Scripts/Utilities/Get-WsusUpdates.ps1
@@ -18,31 +18,80 @@ param(
     [string]$sortBy = "NeededCount" # NeededCount, Classification, IsSuperseded
 )
 
-
-# Get a list of updates
-$updates = Get-WsusUpdate -Approval AnyExceptDeclined -Status Any -Classification All
-
-# Display the table
-$updatesTable = @()
-
-
-foreach ($update in $updates) {
-    $superseded = $false
-    if ($update.UpdatesSupersedingThisUpdate -ne "None") {
-        $superseded = $true
-    }
-    else {
-        $superseded = $false
-    }
-    $updateObject = [PSCustomObject]@{
-        Name = $update.Update.Title
-        Classification = $update.Classification
-        NeededCount = $update.ComputersNeedingThisUpdate
-        IsSuperseded = $superseded
-    }
-    $updatesTable += $updateObject
+function Get-Updates {
+    [CmdletBinding()]
+    $updates = Get-WsusUpdate -Approval AnyExceptDeclined -Status Any -Classification All
+    return $updates
 }
 
+
+function Show-Updates {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [array]$updates # An array of updates
+    )
+
+    # Check if updates were passed in
+    if (!$updates) {
+        $updates = Get-Updates
+    }
+
+    # Construct updates table
+    $updatesTable = @()
+
+    foreach ($update in $updates) {
+        $superseded = $false
+        if ($update.UpdatesSupersedingThisUpdate -ne "None") {
+            $superseded = $true
+        }
+        else {
+            $superseded = $false
+        }
+        $updateObject = [PSCustomObject]@{
+            Name = $update.Update.Title
+            Classification = $update.Classification
+            NeededCount = $update.ComputersNeedingThisUpdate
+            IsSuperseded = $superseded
+        }
+        $updatesTable += $updateObject
+    }
+    return $updatesTable
+}
+
+function Get-FilteredUpdates {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$searchString,
+        [Parameter(Mandatory=$false)]
+        [array]$updates # An array of updates
+    )
+
+    # Check if updates were passed in
+    if (!$updates) {
+        $updates = Get-Updates
+    }
+    # Construct filtered updates table
+    $filteredUpdatesTable = @()
+
+    foreach ($update in $updates) {
+        if (($update.Update.Title | Select-String -Pattern $searchString) -or ($update.Update.Description | Select-String -Pattern $searchString))  {
+            $filteredUpdatesTable += $update
+        }
+    }
+    return $filteredUpdatesTable
+
+}
+
+# Get WSUS server
+#$wsusServer = Get-WsusServer
+
+# Get updates
+$updates = Get-Updates
+$updatesTable = Show-Updates -updates $updates
+
+# Sort updates if parameter is passed
 if ($sortBy) {
     $sortBy = $sortBy.ToLower()
 
@@ -55,4 +104,33 @@ if ($sortBy) {
 }
 else {
     $updatesTable | Format-Table -AutoSize
+}
+
+$loop = $true
+while ($loop) {
+    $searchString = Read-Host "Enter search string (or q to quit)"
+    if ($searchString.ToLower() -eq "q") {
+        $loop = $false
+    }
+    else {
+        $filteredUpdatesTable = Get-FilteredUpdates -updates $updates -searchString $searchString
+        $toDisplay = @()
+        foreach ($update in $filteredUpdatesTable) {
+            $superseded = $false
+            if ($update.UpdatesSupersedingThisUpdate -ne "None") {
+                $superseded = $true
+            }
+            else {
+                $superseded = $false
+            }
+            $updateObject = [PSCustomObject]@{
+                Name = $update.Update.Title
+                Classification = $update.Classification
+                NeededCount = $update.ComputersNeedingThisUpdate
+                IsSuperseded = $superseded
+            }
+            $toDisplay += $updateObject
+        }
+        $toDisplay | Format-Table -AutoSize
+    }
 }
