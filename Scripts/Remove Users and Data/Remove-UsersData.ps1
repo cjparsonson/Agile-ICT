41cb919bd5e6fb8ca5582be8ca5b178caf43d439
cjparsonson <cjparsonson@gmail.com>
2023-09-29 15:04:05 +0100

    Update

--- Scripts/Remove Users and Data/Remove-UsersData.ps1
+++ Scripts/Remove Users and Data/Remove-UsersData.ps1
@@ -1,7 +1,7 @@
 # Read users from list
-$users = Get-Content -Path C:\Users\administrator.SCH2260\userlist.txt
-$homepath = D:\users\homes
-$profilepath = D:\users\profiles\staff
+$users = Get-Content -Path "C:\Users\administrator.SCH2260\userlist.txt"
+$homepath = "D:\users\homes"
+$profilepath = "D:\users\profiles\staff"
 # Iterate through list to build exisiting users
 
 $exisiting_users = @()
@@ -25,7 +25,7 @@ foreach ($user in $exisiting_users) {
 foreach ($user in $users) {
     $homedir = Get-ChildItem -Path $homepath -Filter $user -Recurse -Depth 3 | Select-Object -ExpandProperty FullName
     try {
-        Remove-Item -Path $homedir -Recurse -Force -ErrorAction Stop 
+        Remove-Item -Path $homedir -Recurse -Force -ErrorAction Stop
     }
     catch {
         Write-Warning $_
@@ -36,10 +36,9 @@ foreach ($user in $users) {
 foreach ($user in $users) {
     $profiledir = Get-ChildItem -Path $profilepath -Filter $user -Force | Select-Object -ExpandProperty FullName
     try {
-        Remove-Item -Path $profiledir -Recurse -Force -ErrorAction Stop 
+        Remove-Item -Path $profiledir -Recurse -Force -ErrorAction Stop
     }
     catch {
         Write-Warning $_
     }
 }
-
